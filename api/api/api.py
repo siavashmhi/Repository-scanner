d7d507399ba3ae5ab1201ff55d8f1ed179de7383
siavash mohseni <siavashmohsenimhi79@gmail.com>
2024-08-09 16:57:14 +0330

    add: Add flask application structure

--- api/api/api.py
+++ api/api/api.py
@@ -1,9 +1,6 @@
 from flask import Flask
 
-from .view.v1 import apiv1_bp
-
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(apiv1_bp)
     return app
--- api/api/view/v1/__init__.py
+++ api/api/view/v1/__init__.py
@@ -1,1 +1,27 @@
-from .v1 import api_bp as apiv1_bp
+"""
+    you can create another file for thease codes,
+    but for more simplicity I write these codes in init file.
+"""
+
+from flask import Blueprint
+from flask_restful import Api
+
+from .repository import RepositoryResource
+
+api_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
+api = Api(api_bp)
+
+# create resource
+api.add_resource(
+    RepositoryResource,
+    "/repositories",
+    methods=["GET", "POST"],
+    endpoint="repositories",
+)
+
+api.add_resource(
+    RepositoryResource,
+    "/repositories/<repository_id>",
+    methods=["GET", "PATCH", "DELETE"],
+    endpoint="repository",
+)
