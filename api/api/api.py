71f703597d1e310d6a3bcd59e2c04562d3d65e9b
siavash mohseni <siavashmohsenimhi79@gmail.com>
2024-08-10 04:12:32 +0330

    add: Add database and migrations structure..

--- api/api/api.py
+++ api/api/api.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from .config import Config
 from .view import apiv1_bp
+
+db = SQLAlchemy()
+mg = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    db.init_app(app)
+    mg.init_app(app, db)
     app.register_blueprint(apiv1_bp)
     print(app.config)
     return app
