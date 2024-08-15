72c5717c81bc993aa057ed38901f3e7041ab7d15
siavash mohseni <siavashmohsenimhi79@gmail.com>
2024-08-16 01:09:55 +0330

    complete: GET and POST and UPDATE method in controller.

--- api/api/controller/v1/repository.py
+++ api/api/controller/v1/repository.py
@@ -1,6 +1,9 @@
+from flask import request
+
 from api.model import Repository
+from api.object import db
 from api.schema.v1 import RepositorySchema
-from api.util import jsonify
+from api.util import jsonify, now
 
 
 class RepositoryController:
@@ -9,17 +12,84 @@ class RepositoryController:
             repositories = Repository.query.all()
         except:
             return jsonify(status=500, code=102)
-        repositories_schema = RepositorySchema(many=True)
+
+        try:
+            repositories_schema = RepositorySchema(many=True)
+        except:
+            return jsonify(status=500, code=103)
         return jsonify(repositories_schema.dump(repositories))
 
-    def get_repository(repository_id): 
-        return jsonify(status=501, code=101)
+    def get_repository(repository_id):
+        try:
+            repository = Repository.query.get(repository_id)
+        except:
+            return jsonify(status=500, code=102)
+        if repository is None:
+            return jsonify(status=500, code=105)
+
+        try:
+            repository_schema = RepositorySchema()
+        except:
+            return jsonify(status=500, code=103)
+
+        return jsonify(repository_schema.dump(repository))
 
     def create_repository():
-        return jsonify(status=501, code=101)
+        try:
+            repository_schema = RepositorySchema()
+        except:
+            return jsonify(status=500, code=103)
+
+        try:
+            request_data = repository_schema.load(request.get_json())
+        except:
+            return jsonify(status=500, code=104)
+
+        repository = Repository(
+            owner="TBC",
+            url=request_data["url"],
+        )
+        db.session.add(repository)
+
+        try:
+            # insert data to databse
+            db.session.commit()
+        except:
+            db.session.rollback()
+            return jsonify(status=500, code=102)
+        return jsonify(repository_schema.dump(repository), status=201)
 
     def update_repository(repository_id):
-        return jsonify(status=501, code=101)
+        try:
+            repository = Repository.query.get(repository_id)
+        except:
+            return jsonify(status=500, code=102)
+        if repository is None:
+            return jsonify(status=500, code=105)
+
+        try:
+            repository_schema = RepositorySchema(only=["status"])
+        except:
+            return jsonify(status=500, code=103)
+
+        try:
+            request_data = repository_schema.load(request.get_json())
+        except:
+            return jsonify(status=500, code=104)
+        repository.status = request_data["status"]
+        repository.last_update_at = now()
+        try:
+            # insert data to databse
+            db.session.commit()
+        except:
+            db.session.rollback()
+            return jsonify(status=500, code=102)
+
+        try:
+            repository_schema = RepositorySchema()
+        except:
+            return jsonify(status=500, code=103)
+        return jsonify(repository_schema.dump(repository))
 
     def delete_repository(self, repository_id):
         return jsonify(status=501, code=101)
--- api/api/util/jsonify.py
+++ api/api/util/jsonify.py
@@ -2,10 +2,11 @@ from flask import current_app
 
 APP_CODES = {
     100: "ok",
-    101: "Feature is not implemented.", 
+    101: "Feature is not implemented.",
     102: "Database error.",
-    103: "Schema validation failed.",
-    104: "Schema instance error.",
+    103: "Schema instance error.",
+    104: "Input validation failed.",
+    105: "Resource not found.",
 }
 
 
