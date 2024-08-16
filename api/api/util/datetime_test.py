8ffe0fc54aef21a4c654fe0bcd4695b71312172d
siavash mohseni <siavashmohsenimhi79@gmail.com>
2024-08-17 01:01:56 +0330

    add: datetime_test.py file for test datetime.py file.

---
+++ api/api/util/datetime_test.py
@@ -0,0 +1,15 @@
+import pytest
+
+from api.config import Config
+from .datetime import now
+from datetime import datetime
+from pytz import timezone
+
+def test_now():
+    app_result = now()
+    sys_result = datetime.now(tz=timezone(Config.TIMEZONE))
+    assert app_result.tzinfo.zone == Config.TIMEZONE
+    assert app_result.year == sys_result.year
+    assert app_result.month == sys_result.month
+    assert app_result.day == sys_result.day
+    assert app_result.hour == sys_result.hour
