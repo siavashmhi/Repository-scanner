91a588527d87f75dd9fcf13900f18b4421d9e7c5
siavash mohseni <siavashmohsenimhi79@gmail.com>
2024-08-19 17:00:43 +0330

    create: config file for pytest.

--- api/api/conftest.py
+++ api/api/conftest.py
@@ -1,11 +1,43 @@
 import pytest
 
+from api.object import db as database
+from api.object import mg as migration
+
 from .api import create_app
+
+
+class TestStorage:
+    def set(self, name, value):
+        setattr(self, name, value)
+        return True
+
+    def get(self, name):
+        return getattr(self, name, None)
+
+
+test_storage = TestStorage()
+
+
+@pytest.fixture
+def storage():
+    return test_storage
+
 
 @pytest.fixture
 def app():
     return create_app()
 
+
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture
+def db():
+    return database
+
+
+@pytest.fixture
+def mg():
+    return migration
